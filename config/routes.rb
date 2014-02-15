22bd55c5b62492dc131243c01cdc8eccbf7910ea
Mike Adeleke <madeleke13@gmail.com>
2014-02-15 08:03:08 +0000

    tracks can now be added

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,7 @@ HackerGenius::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tracks#index'
   resources :tracks
   
   
-  devise_for :users, :skip => [:sessions]
-  as :user do
-    get 'signin' => 'devise/sessions#new', :as => :new_user_session
-    post 'signin' => 'devise/sessions#create', :as => :user_session
-    match 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session,
-      :via => Devise.mappings[:user].sign_out_via
-  end
 end
